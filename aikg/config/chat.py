a3158bb68d00a8318c08c78b4b14c9da5ce4b9c2
cyril.matthey-doret <cyril.matthey-doret@epfl.ch>
2023-05-15 18:09:45 +0200

    feat: remove notion of triples from prompt template

--- aikg/config/chat.py
+++ aikg/config/chat.py
@@ -9,6 +9,17 @@ class Location(BaseModel):
 
 
 class Config(BaseModel):
+    """Chatbot configuration.
+
+    Attributes:
+        model_id: The HuggingFace ID of the model to use for text generation.
+        max_new_tokens: The maximum number of tokens to generate.
+        max_input_size: The maximum number of tokens in the input.
+        num_output: The number of outputs to generate.
+        max_chunk_overlap: The maximum number of tokens to overlap between chunks.
+        prompt_template: The template for the prompt to inject into the model. The template should contain the following variables: context_str, query_str.
+    """
+
     model_id: str = "chainyo/alpaca-lora-7b"
     max_new_tokens: int = 256
     max_input_size: int = 2048
@@ -16,15 +27,14 @@ class Config(BaseModel):
     max_chunk_overlap: int = 20
 
     prompt_template: str = (
-        "We have provided the contextual facts below in the form of "
-        "<subject> <predicate> <object> triples.\n"
+        "We have provided the contextual facts below.\n"
         "-----------------\n"
         "{context_str}\n"
         "-----------------\n"
         "Answer the question using only the context and no "
-        "prior knowledge. If the context does not contain any triple related to "
+        "prior knowledge. If the context does not contain any fact related to "
         "the question, simply answer the words 'Not found'. The answer should be "
-        "up to 2 sentences directly reflecting the facts from relevant triples while ignoring "
+        "up to 2 sentences directly reflecting the facts from relevant facts while ignoring "
         "irrelevant ones.\n"
         "Question: {query_str}"
         "Answer: "
