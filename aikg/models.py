c69fc93805f748605fa1668c7b26348264be0742
cyril.matthey-doret <cyril.matthey-doret@epfl.ch>
2023-05-15 18:10:38 +0200

    fix: handle none timestamps in conversation model

--- aikg/models.py
+++ aikg/models.py
@@ -11,19 +11,30 @@ class Message(BaseModel):
 
 
 class Conversation(BaseModel):
+    """A conversation, represented as a list of messages
+    and a unique identifier (uid)."""
+
     thread: list[Message]
     uid: str | None = str(uuid.uuid4())
 
     @property
-    def start_time(self) -> datetime:
-        return self.thread[0].time
+    def start_time(self) -> datetime | None:
+        try:
+            return self.thread[0].time
+        except IndexError:
+            return None
 
     @property
-    def end_time(self) -> datetime:
-        return self.thread[-1].time
+    def end_time(self) -> datetime | None:
+        try:
+            self.thread[-1].time
+        except IndexError:
+            return None
 
     @property
-    def duration(self) -> timedelta:
+    def duration(self) -> timedelta | None:
+        if self.start_time is None or self.end_time is None:
+            return None
         return self.end_time - self.start_time
 
     @property
