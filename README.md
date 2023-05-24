3e2ecb4cb9355d8cb4cbd110d35c4bbe5f9b58e4
Cyril Matthey-Doret <cyril.matthey-doret@epfl.ch>
2023-05-24 13:58:31 +0000

    feat: Add Dockerfil and docker-compose setup (#3)
    * fix: rm debug logging from fastapi server
    
    * feat: add docker compose template
    
    * refactor: adapt+extend chromadb docker-compose
    
    * doc: README instructions for WIP compose setup
    
    * feat: use profiles and shared network in compose
    
    * feat: add example .env file
    
    * doc: add docker compose viz in readme
    
    * chore: add uvicorn to deps
    
    * fix(compose): run uvicorn for chat-server in poetry
    
    * refactor: separate config fields for chroma host and port
    
    * fix: dockerfile entrypoint for server
    
    * chode(config): add default values in .env.example
    
    * fix(config): chromadb collection from env variable
    
    * fix(docker): add missing dependencies in server dockerfile
    
    * feat(docker): read ports from .env in compose file
    
    * chore(docker): pin poetry version in Dockerfile
    
    * fix(deps): cpu-only torch distribution
    
    * doc: fix example usage command
    
    * fix(docker): pass .env file to chat-server service
    
    * fix: mismatched chroma host name between compose and env example

--- README.md
+++ README.md
@@ -26,3 +26,39 @@ CLI usage: `python aikg/flows/chroma_build.py location.yml`
 The chat server can be started by running:
 
 `uvicorn aikg.server:app`
+
+## Containerized service
+
+:warning: WIP, not functional yet
+
+All services can be deployed together using `docker-compose`. The compose setup is as follows:
+
+```mermaid
+%%{init: {'theme': 'default'}}%%
+flowchart TB
+  chatserver[chat-server] -->|depends on| chromaserver[chroma-server]
+  chromaserver -->|depends on| clickhouse
+  P0((8001)) -.-> chatserver
+  P1((8000)) -.-> chromaserver
+  P2((8123)) -.-> clickhouse
+  P3((8999)) -.-> clickhouse
+  chatserver -.- net[/net/]
+  chromaserver -.- net
+  clickhouse -.- net
+
+  classDef ports fill:#f8f8f8,stroke:#ccc
+  class P0,P1,P2,P3 ports
+  classDef nets fill:#fbfff7,stroke:#8bc34a
+  class net nets
+```
+> figure generated with [docker-compose-viz-mermaid](https://github.com/derlin/docker-compose-viz-mermaid)
+
+```sh
+docker-compose --profile db up --build
+```
+
+If an external instance of ChromaDB is already running, set the correct values for CHROMA_HOST and CHROMA_PORT in `.env`, then run:
+
+```sh
+docker-compose up --build
+```
--- aikg/server.py
+++ aikg/server.py
@@ -2,6 +2,7 @@
 fetches context for that question in a vector store and injects them into a prompt.
 It then sends the prompt to a LLM and returns the response to the client.
 """
+from base64 import urlsafe_b64decode
 import logging
 import sys
 
@@ -11,7 +12,6 @@ from datetime import datetime
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from langchain import HuggingFacePipeline, LLMChain, PromptTemplate
-from llama_index import QuestionAnswerPrompt, GPTVectorStoreIndex
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 import aikg.config.chroma
@@ -53,7 +53,7 @@ def setup_chroma() -> Collection:
     embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(
         model_name=chroma_config.embedding_model
     )
-    client = get_chroma_client(chroma_config.chroma_url)
+    client = get_chroma_client(chroma_config.host, chroma_config.port)
     collection = client.get_collection(
         chroma_config.collection_name, embedding_function=embedding_function
     )
@@ -66,15 +66,11 @@ def synthesize(
     """Retrieve k-nearest documents from the vector store and synthesize
     an answer using documents as context."""
 
-    logging.debug(f"Question: {query}")
     results = collection.query(query_texts=query, n_results=limit)
     context = "\n".join(results["documents"][0])
-    logging.debug(f"Context: {context}")
     triples = "\n".join([res.get("triples", "") for res in results["metadatas"][0]])
-    logging.debug(f"Triples: {triples}")
     answer = llm_chain.run(query_str=query, context_str=context)
     answer = post_process_answer(answer)
-    logging.debug(f"Answer: {answer}")
     return Message(text=answer, triples=triples, sender="AI", time=datetime.now())
 
 
