62a1db170b518d14852f49bb9848b000e9d0bbc0
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2024-02-12 14:28:16 +0100

    fix: init containers in kg-llm

--- k8s/overlays/custom-config/entrypoint.sh
+++ k8s/overlays/custom-config/entrypoint.sh
@@ -19,4 +19,5 @@ head -n 1000 poke-a.nq > poke-a-c.nq
 cd ..
 poetry run python3 aikg/flows/insert_triples.py /app/temp/ontology.nt
 poetry run python3 aikg/flows/insert_triples.py /app/temp/poke-a-c.nq
+
 poetry run python3 aikg/flows/chroma_build.py
